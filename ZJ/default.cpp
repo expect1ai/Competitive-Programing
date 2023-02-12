2ba6c82e2f6d2a13f606aa519e8de514d68413cc
expect1ai <expect1ai@outlook.com>
2023-02-12 23:11:34 +0800

    test

--- ZJ/default.cpp
+++ ZJ/default.cpp
@@ -4,6 +4,9 @@
 #define int long long
 #define endl '\n'
 #define fastio ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define first f
+#define second s
+#define push_back pb
 using namespace std;
 signed main() {
     fastio;
