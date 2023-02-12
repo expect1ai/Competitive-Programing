ccd144893495bc5698147caf46ef46d10237d469
expectlai <expectlai@gmail.com>
2023-02-12 15:32:01 +0800

    default code

---
+++ ZJ/default.cpp
@@ -0,0 +1,11 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define fastio ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+using namespace std;
+signed main() {
+    fastio;
+    
+}
